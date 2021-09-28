298e9ae6b6a7bb97518306598fa0941087c66f8d
Kyle Harrington <mdc@kyleharrington.com>
2021-09-28 14:40:23 +0200

    Fix initial point size in point_cloud_demo

--- scripts/point_cloud_demo.py
+++ scripts/point_cloud_demo.py
@@ -4,7 +4,7 @@ from net.imglib2 import RealPoint
 from jarray import array
 import random as random
 
-w = 101
+w = 1.0
 h = w
 d = w
 
@@ -12,7 +12,7 @@ num_spots = 10
 spotR = 3
 
 def rand_spot():
-    coord = [random.randint(spotR, w-spotR), random.randint(spotR, h-spotR), random.randint(spotR, d-spotR)]
+    coord = [random.random() * w - w * 0.5, random.random() * h - h * 0.5, random.random() * d - d * 0.5]
     return RealPoint(array(coord, 'd'))
 
 spots = [rand_spot() for k in range(num_spots)]
